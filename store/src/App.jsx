fddae90b1faeba36ba3babdb939a9c3823c57071
ItLena <ebordinghaus@gmail.com>
2024-10-22 13:46:03 +0200

    initialize store app

---
+++ store/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <body>
+      <header>Welcome in grossery store</header>
+    </body>
+    
+    </>
+  )
+}
+
+export default App
