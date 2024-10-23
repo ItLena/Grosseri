1a1fe3e7286814a4a9d73095670753aa9c5296a2
ItLena <ebordinghaus@gmail.com>
2024-10-23 09:51:24 +0200

    icons + searchbar

--- store/src/components/Navbar/Navbar.jsx
+++ store/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import {Link} from 'react-router-dom';
 import './style.css'
+import { TiShoppingCart } from "react-icons/ti";
+import SearchBar from '../SearchBar'
 
 const Navbar = () =>{
     return(
@@ -8,7 +10,8 @@ const Navbar = () =>{
             <ul>
                 <li><Link to="/"/>Home</li>
                 <li><Link to="/login"/>Login</li>
-                <li><Link to="/basket"/>Customer basket</li>
+                <li><SearchBar/></li>
+                <li><Link to="/basket"/><TiShoppingCart className="icon"/></li>
             </ul>
         </nav>
         </>
---
+++ store/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+const SearchBar = () =>{
+    return(
+        <>
+        <input 
+        type="search" 
+        placeholder="Search product"
+        />
+        </>
+    )
+}
+
+export default SearchBar;
